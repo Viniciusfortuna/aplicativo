962d9c06a7ffb7239ae98fdfdc76edeb55c4f915
Viniciusfortuna <vini.s.fortuna@gmail.com>
2025-02-22 15:19:43 -0300

    :sparkles: identação

--- src/app/functions/services/situations/sincronizaSituacao.js
+++ src/app/functions/services/situations/sincronizaSituacao.js
@@ -1,38 +1,63 @@
 import servicesSituation from "./servicesSituation";
 import sync_situation from "./serviceSyncS";
 
+export default async function SincronizaSituacao() {
+  //Verifica os clientes que não existem na API, e deleta
+  try {
+    const data_off = await servicesSituation("SELECT", "situacao", "ALL", "");
+    data_off.forEach(async (item) => {
+      const result = await sync_situation("GET", "", "ID", item.codsit);
+      if (!result) {
+        const result = servicesSituation(
+          "DELETE",
+          "situacao",
+          "",
+          item.codsit,
+          "",
+          ""
+        );
+        console.log(result);
+      }
+      console.log(item.codsit);
+    });
 
-export default async function SincronizaSituacao(){
-    //Verifica os clientes que não existem na API, e deleta
-    try {
-        const data_off = await servicesSituation('SELECT', 'situacao', 'ALL', '');
-        data_off.forEach(async (item) =>{
-            const result = await sync_situation('GET', '', 'ID', item.codsit);
-            if(!result){
-                const result = servicesSituation('DELETE', 'situacao', '', item.codsit, '', '');
-                console.log(result)
-            }
-            console.log(item.codsit)
+    const data = await sync_situation("GET", "");
+    if (data.length > 0) {
+      data.forEach((item) => {
+        const result = servicesSituation(
+          "SELECT",
+          "situacao",
+          "ID",
+          item.codsit,
+          "",
+          ""
+        );
+        result.then((value) => {
+          if (value.length > 0) {
+            const result = servicesSituation(
+              "UPDATE",
+              "situacao",
+              "",
+              item,
+              "",
+              ""
+            );
+            console.log(result);
+          } else {
+            const result = servicesSituation(
+              "INSERT",
+              "situacao",
+              "",
+              item,
+              "",
+              ""
+            );
+          }
         });
-
-        const data = await sync_situation('GET', ''); 
-        if (data.length > 0) {
-            data.forEach((item) => {
-            const result = servicesSituation('SELECT', 'situacao', 'ID', item.codsit, '', '');
-            result.then((value)=>{
-                if(value.length > 0){
-                    const result = servicesSituation('UPDATE', 'situacao', '', item, '', '');
-                    console.log(result)
-                }
-                else {
-                    const result = servicesSituation('INSERT', 'situacao', '', item, '', '');
-                }
-            })
-            });
-        }
-        return 'ok'
-    } catch (error) {
-        return error;
+      });
     }
-
+    return "ok";
+  } catch (error) {
+    return error;
+  }
 }
--- src/app/functions/services/users/sincronizaUsuarios.js
+++ src/app/functions/services/users/sincronizaUsuarios.js
@@ -2,38 +2,49 @@ import { useEffect, useState } from "react";
 import servicesUsers from "./servicesUser";
 import sync_users from "./serviceSyncU";
 
+export default async function SincronizaUsuarios() {
+  //Verifica os clientes que não existem na API, e deleta
+  try {
+    const data_off = await servicesUsers("SELECT", "users", "ALL", "");
+    data_off.forEach(async (item) => {
+      const result = await sync_users("GET", "", "ID", item.codusu);
+      if (!result) {
+        const result = servicesUsers(
+          "DELETE",
+          "users",
+          "",
+          item.codusu,
+          "",
+          ""
+        );
+        console.log(result);
+      }
+      console.log(item.codusu);
+    });
 
-export default async function SincronizaUsuarios(){
-    //Verifica os clientes que não existem na API, e deleta
-    try {
-        const data_off = await servicesUsers('SELECT', 'users', 'ALL', '');
-        data_off.forEach(async (item) =>{
-            const result = await sync_users('GET', '', 'ID', item.codusu);
-            if(!result){
-                const result = servicesUsers('DELETE', 'users', '', item.codusu, '', '');
-                console.log(result)
-            }
-            console.log(item.codusu)
+    const data = await sync_users("GET", "");
+    if (data.length > 0) {
+      data.forEach((item) => {
+        const result = servicesUsers(
+          "SELECT",
+          "users",
+          "ID",
+          item.codusu,
+          "",
+          ""
+        );
+        result.then((value) => {
+          if (value.length > 0) {
+            const result = servicesUsers("UPDATE", "users", "", item, "", "");
+            console.log(result);
+          } else {
+            const result = servicesUsers("INSERT", "users", "", item, "", "");
+          }
         });
-
-        const data = await sync_users('GET', '');
-        if (data.length > 0) {
-            data.forEach((item) => {
-            const result = servicesUsers('SELECT', 'users', 'ID', item.codusu, '', '');
-            result.then((value)=>{
-                if(value.length > 0){
-                    const result = servicesUsers('UPDATE', 'users', '', item, '', '');
-                    console.log(result)
-                }
-                else {
-                    const result = servicesUsers('INSERT', 'users', '', item, '', '');
-                }
-            })
-            });
-        }
-        return 'ok'
-    } catch (error) {
-        return error;
+      });
     }
-
+    return "ok";
+  } catch (error) {
+    return error;
+  }
 }
